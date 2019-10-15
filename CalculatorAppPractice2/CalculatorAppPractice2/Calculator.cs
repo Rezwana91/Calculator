3e5bc9dbf561ff16e6acf1d8c9a207f545d1ab93
Rezwana91 <rrananna@gmail.com>
2019-10-15 08:54:07 +0600

    added

---
+++ CalculatorAppPractice2/CalculatorAppPractice2/Calculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorAppPractice2
+{
+    public class Calculator
+    {
+        private double Result;
+
+        public double Add(double firstNumber,double secondNumber)
+        {
+            Result = firstNumber + secondNumber;
+
+            return Result;
+        }
+
+        public double Subtract(double firstNumber, double secondNumber)
+        {
+            Result = firstNumber - secondNumber;
+
+            return Result;       
+        }
+
+        public double Multiply(double firstNumber, double secondNumber)
+        {
+            Result = firstNumber * secondNumber;
+
+            return Result;
+        }
+
+
+        public double Divide(double firstNumber, double secondNumber)
+        {
+            Result = firstNumber/secondNumber;
+
+            return Result;
+        }
+
+
+    }
+}
